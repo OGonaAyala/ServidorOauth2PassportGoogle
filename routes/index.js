c7ff39d5ca821cf2370676fedae9e08aea9002a7
OGaonaAyala <gaonaayalaomar@gmail.com>
2019-08-01 11:11:12 -0500

    Se eliminaron algunos parametros innecesarios que se enviaban en la url de redireccion despues de autenticacion

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,31 @@
 var express = require('express');
 var router = express.Router();
-var passport = require ("passport");
+var passport = require('passport');
 
 /* GET Google Authentication API. */
 router.get(
-    "/auth/google",
-    passport.authenticate("google", {accessType: 'offline', scope: ["profile", "email", "https://www.googleapis.com/auth/drive.file"]})
+  '/auth/google',
+  passport.authenticate('google', {
+    accessType: 'offline',
+    scope: ['profile', 'email', 'https://www.googleapis.com/auth/drive.file'],
+  }),
 );
 router.get(
-    "/auth/google/callback",
-    passport.authenticate("google", { failureRedirect: "http://localhost:3000", session: false }),
-    function(req, res) {
-        var token = req.user.token;
-        var refreshToken = req.user.refreshToken;
-        console.log(req.user); 
-        res.redirect("http://localhost:3000/googleSucces#token=" + token);
-    }
+  '/auth/google/callback',
+  passport.authenticate('google', {
+    failureRedirect: 'http://localhost:3000',
+    session: false,
+  }),
+  function(req, res) {
+    const token = req.user.token;
+    const refreshToken = req.user.refreshToken;
+    res.redirect(
+      'http://localhost:3000/googleSucces#token=' +
+        token +
+        '&refreshToken=' +
+        refreshToken,
+    );
+  },
 );
 
 module.exports = router;
